9049b02d67d94912b0f4af7b1b28323130a02942
Jose Chaves Arias <jose.chaves11@ulatina.net>
2017-05-29 20:23:09 -0600

    Semana 4 - 29/05/2017 Se agrega proyecto de Logica de Negocio.

---
+++ Ulatina.PrograAvanzada.Calentamiento/Ulatina.PrograAvanzada.BLAplicacionWeb/Dominio/Acciones/ObtenerFechaActual.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.PrograAvanzada.BLAplicacionWeb.Dominio.Acciones
+{
+    public class ObtenerFechaActual
+    {
+        public string ObtenerFecha()
+        {
+            var laEspecificacion = new Especificaciones.ObtengaFecha();
+            var laFecha = laEspecificacion.FormatearUnaFecha(DateTime.Now, "dd/mm/yyyy");
+            return laFecha;
+        }
+    }
+}
