6203daa29e76a689f7d46bc7454cd4cf60db0f52
soportetecnicolatina <soportetecnicolatina@outlook.es>
2017-06-26 20:39:40 -0600

    Clase 7

---
+++ Ulatina.PrograAvanzada.Calentamiento/Ulatina.PrograAvanzada.MatricesWcf/Dominio/Acciones/Transponer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Acciones
+{
+    internal class Transponer
+    {
+        public Transponer()
+        {
+        }
+
+        public double[,] CalcularTranspuesta(double[,] laMatriz)
+        {
+            double[,] elResultado;
+
+            var elCalculo = new Especificaciones.CalculoTranspuesta(laMatriz);
+            elResultado = elCalculo.elCalculoDeLaTranspuesta();
+
+            return elResultado;
+        }
+    }
+}
---
+++ Ulatina.PrograAvanzada.Calentamiento/Ulatina.PrograAvanzada.MatricesWcf/Dominio/Especificaciones/CalculoTranspuesta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Especificaciones
+{
+    internal class CalculoTranspuesta
+    {
+        private double[,] laMatriz;
+
+        public CalculoTranspuesta(double[,] laMatriz)
+        {
+            this.laMatriz = laMatriz;
+        }
+
+        internal double[,] elCalculoDeLaTranspuesta()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
